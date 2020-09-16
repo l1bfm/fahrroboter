95b04cb8a9ca29eb0bca25caf78682a2dc2cda1e
Florentin Möhle <libfmx@gmail.com>
2020-09-16 21:45:01 +0200

    Kommentare zu Programm/main.cpp hinzugefügt

--- Programm/main.cpp
+++ Programm/main.cpp
@@ -11,6 +11,7 @@ int Schwarz = 1023;
 int Weiss = 0;
 
 void setup() {
+	//Motorpins als Output deklarieren
 	for(int i=0; i<=1; i++) {
 		pinMode(motor[i], OUTPUT);
 		pinMode(motor[i]+1, OUTPUT);
@@ -21,11 +22,14 @@ void setup() {
 }
 
 void loop() {
+	//LDR-Werte auslesen
 	int LDR_Werte [2] = ldr_read();
+	//Linienfolger ausführen
 	linien_drive(LDR_Werte);
 }
 
 int ldr_read() {
+	//Array der Werte einlesen und dann returnen
 	int Werte [2] = { analog.Read(LDR_links), analog.Read(LDR_rechts) };
 	return Werte;
 }
@@ -34,27 +38,34 @@ void linien_drive(LDR_Werte) {
 	int fahr_werte [2];
 	int fahr_werte_diff [2];
 	int vorwaerts = 1;
+	// Map
 	for(int i=0; i<=1; i++) {
 		fahr_werte[i] = map(LDR_Werte[i], Weiss, Schwarz, 510, 0) - 255;
 		fahr_werte_diff[i] = 255 - fahr_werte[i]
 	}
+	// Maximierung der Geschwindigkeit
 	sort(fahr_werte_diff, fahr_werte_diff + 5);
 	for(int i=0; i<=1; i++) {
 		fahr_werte[i] += fahr_werte_diff[0];
+		// gemapptes zu zwei-pin-Daten
 		if(fahr_werte[i] < 0) {
 			fahr_werte[i] *= -1;
 			vorwaerts = 0;
 		}
+		// Für jede Seite Motor ansteuern
 		analogWrite(motor[i], 0);
 		analogWrite(motor[i]+1, 0);
 		analogWrite(motor[i]+vorwaerts, fahr_werte[i]);
 	}
-	
+
 }
 
 void kalibrierung() {
+	// Während der Knopf nicht gedrückt ist
 	while(digitalRead(pushbutton) == 0) {
+		// LDR lesen
 		int LDR_Werte [2] = ldr_read();
+		// Werte ausgeben
 		Serial.print(LDR_Werte[0]);
 		Serial.print("\t");
 		serial.print(LDR_Werte[1]);
